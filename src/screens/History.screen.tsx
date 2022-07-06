a967c0546a225bd428cb8c54523317540f0692fd
Andrew Hudson <ahudson@gmail.com>
2022-07-06 17:45:42 +0100

    stuff

--- src/screens/History.screen.tsx
+++ src/screens/History.screen.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, ScrollView } from 'react-native';
 import { useAppContext } from '../components/App.provider';
 import { MoodItemRow } from '../components/MoodItemRow';
 
 export const History: React.FC = () => {
   const { moodList } = useAppContext();
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       {moodList.length > 0 &&
         moodList
           .slice()
           .reverse()
           .map(item => <MoodItemRow item={item} key={item.timestamp} />)}
-    </View>
+    </ScrollView>
   );
 };
 
